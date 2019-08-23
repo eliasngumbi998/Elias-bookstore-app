079bcd88f2fde03a65a4bd18b5d7db20417bed7c
ELIAS NGUMBI <32370330+eliasngumbi998@users.noreply.github.com>
2019-08-23 15:24:33 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Elias Bookstore</title>
+</head>
+<body>
+<h2>Welcome to Elias Bookstore</h2>
+<form method="GET" action="books.php">
+<table>
+<tr>
+<td>Firstname:</td>
+<td><input type="text" name="fname"/></td>
+</tr>
+<tr>
+<td>Lastname:</td>
+<td><input type="text" name="lname"/></td>
+</tr>
+<tr>
+<td><input type="submit" name="Loging" value="Login"/></td>
+<td><input type="reset" name="cancel" value="Clear"/></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
